888115848e55609e1ad7e0be92f2266283be5ad1
Amrani-Anas <lguerd_forever@hotmail.com>
2020-07-15 18:06:08 +0100

    envoie des fichiers existants

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { StyleSheet, Text, View,Image,  TextInput, ScrollView, TouchableHighlight } from 'react-native';
 
 export default function App() {
+  const apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=b2b463b9"
+  const [state, setState] = useState({
+    s: "Movie name...",
+    results: [], 
+    selected: {}
+  });
+  const search = () => {
+    axios(apiUrl + "&s=" + state.s).then(({data}) => {
+      let results = data.Search;
+      
+      setState(prevState => {
+        return{ ...prevState, results: results}
+      })
+    })
+  }
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Find Movies </Text>
+      <TextInput 
+      style={styles.searchbox}
+      onChangeText={text => setState(prevState => {
+        return {...prevState, s: text}
+      })}
+      onSubmitEditing={search}
+      value={state.s}
+      />
+      <ScrollView style={styles.results}>
+        {state.results.map(result => (
+       
+          <View  style={styles.result}>
+              <Text style={styles.heading}>{result.Title}</Text>
+              <Image
+              source={{ uri: result.Poster }}
+              style={{
+                width: '100%',
+                height: 300
+              }}
+              resizeMode="cover"
+              />
+              <Text style={styles.heading}>Rating : 7.2 </Text>
+          </View>
+          
+        ))}       
+      </ScrollView>
     </View>
   );
 }
@@ -14,8 +56,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#223343',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 70,
+    paddingHorizontal: 20
   },
+  title: {
+    color: '#FFF',
+    fontSize: 32, 
+    fontWeight: '700',
+    textAlign: 'center',
+    marginBottom: 20
+  }, 
+  searchbox: {
+    fontSize: 20, 
+    fontWeight: '300', 
+    padding: 20,
+    width: '100%',
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    marginBottom: 40
+  },
+  results: {
+    flex: 1
+  }, 
+  result: {
+    flex: 1, 
+    width: '100%',
+    marginBottom: 20
+  },
+  heading: {
+    color: '#FFF', 
+    fontSize: 18,
+    fontWeight: '700',
+    padding: 20, 
+    backgroundColor: '#445565'
+  }
 });
